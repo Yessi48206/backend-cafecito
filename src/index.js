813a7be9b4990b3f72d236e7cf432701cefbffed
Yessica Juarez <yessi48206@gmail.com>
2022-11-10 17:31:58 -0300

    agregamos las midlewares

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 
 //creamos una instancia de express
 
 const app= express();
 //crear la variable que va a tener el puerto
 //process es un objeto de node
-app.set('port', process.env.PORT  || 4000)
+app.set('port', process.env.PORT  || 4100)
 
 //para usar el puerto o variable
 app.listen(app.get('port'), ()=>{
     console.log('Estoy en el puerto' + app.get(' port'))
 });
 
+//agregar las funciones q se ejecuta antes de las rutas
+//midlewares
 
-console.log('hola mundo')
+app.use(cors()); //permite conexiones remota
+//permite interpretar el formato json
+app.use(express.json());
+app.use(express.urlencoded({extends:true}));
+
+//las rutas 
