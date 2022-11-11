ef8d5ff12e98ef88b847c56630d05a54a1fce540
Yessica Juarez <yessi48206@gmail.com>
2022-11-11 14:18:23 -0300

    agregamos los diddlewares morgan y static

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import path from 'path';
 
 //creamos una instancia de express
 
 const app= express();
 //crear la variable que va a tener el puerto
 //process es un objeto de node
-app.set('port', process.env.PORT  || 4100)
+app.set('port', process.env.PORT  || 5500)
 
 //para usar el puerto o variable
 app.listen(app.get('port'), ()=>{
@@ -21,5 +22,18 @@ app.use(cors()); //permite conexiones remota
 //permite interpretar el formato json
 app.use(express.json());
 app.use(express.urlencoded({extends:true}));
+app.use(morgan('dev'));
+//cargar un archivo estatico
+//console.log(path.join(__dirname, '../public'));
+app.use(express.static(path.join(__dirname, '../public')))
 
 //las rutas 
+//http://localhost:5500/prueba
+app.get('/prueba',(req,res)=>{
+res.send('esto es una prueba de una peticion get');
+
+})
+app.delete('/prueba',(req,res)=>{
+    res.send('esto es una prueba de una peticion delete');
+    
+    })
