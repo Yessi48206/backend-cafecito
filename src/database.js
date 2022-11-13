25ae106413c50ba4b95914bd641d39a455bd8be0
Yessica Juarez <yessi48206@gmail.com>
2022-11-13 14:48:14 -0300

    agregamos la BD a la producion

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
 //const url = 'mongodb://localhost:27017/cafecito'
-const url = 'mongodb://127.0.0.1:27017/cafecito';
-
+//const url = 'mongodb://127.0.0.1:27017/cafecito'; BAS DE DATO LOCAL
+const url = 'mongodb+srv://Yessi1999:Jorge1999@cluster0.olmkxwt.mongodb.net/cafecito-c8i';
 mongoose.connect(url);
 
 const conexion= mongoose.connection;
